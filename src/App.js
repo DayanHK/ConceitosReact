dedbc9ed8ccc83fc2a2157411b3d4a5433f669c5
Dayan Hinterholtz Kirsten <dayan@daksolucoes.com.br>
2020-07-21 06:38:39 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import api from "./services/api";
+const { isUuid } = require("uuidv4");
 
 import "./styles.css";
 
 function App() {
+  function validateId(request, response, next){
+    const { id } = request.params;
+    if(!isUuid(id)){
+      return response.status(400).json({ Error : 'Incorrect ID' });
+    }
+    next();
+  }
+  app.use("/repositories/:id", validateId);
+
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get("repositories").then(response =>{
+        setRepositories(response.data);
+    })
+  });
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", title, techs)
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+    if (repositoryIndex < 0 ){
+      return response.status(400).json({error:'should not be able to delete a repository that does not exist'});
+    }
+    repositories.splice(repositoryIndex, 1);
+    return response.status(204).send();
   }
 
   return (
